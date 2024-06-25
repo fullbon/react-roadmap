aa3d9cccbf85709a830dc51fcf5172009d123a05
Бондарь Артем Андреевич <bondar.aa@regenergy.ru>
2024-06-25 14:39:02 +0300

    es6

--- src/app/lib/links.tsx
+++ src/app/lib/links.tsx
@@ -42,69 +42,89 @@ const links = [
                             {
                                 name: 'ES6 (2015)',
                                 href: '/js/introduction-to-js/versions-of-js/es6',
-                                status: 'at-work'
                             },
                             {
                                 name: 'ES7 (2016)',
                                 href: '/js/introduction-to-js/versions-of-js/es7',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES8 (2017)',
                                 href: '/js/introduction-to-js/versions-of-js/es8',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES9 (2018)',
                                 href: '/js/introduction-to-js/versions-of-js/es9',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES10 (2019)',
                                 href: '/js/introduction-to-js/versions-of-js/es10',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES11 (2020)',
                                 href: '/js/introduction-to-js/versions-of-js/es11',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES12 (2021)',
                                 href: '/js/introduction-to-js/versions-of-js/es12',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES13 (2022)',
                                 href: '/js/introduction-to-js/versions-of-js/es13',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES14 (2023)',
                                 href: '/js/introduction-to-js/versions-of-js/es14',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
                             {
                                 name: 'ES15 (2024)',
                                 href: '/js/introduction-to-js/versions-of-js/es15',
-                                status: 'to-do'
+                                status: 'to-do',
                             },
-                        ]
+                        ],
                     },
                     {
                         name: '1.4. Как запустить JS?',
                         href: '/js/introduction-to-js//how-to-run-js',
                     },
-                ]
+                ],
             },
             {
                 name: '2. Переменные',
                 href: '/js/all-about-variables',
-                status: 'at-work'
-            }
+                status: 'at-work',
+                subLinks: [
+                    {
+                        name: 'Объявление переменных',
+                        href: '/js/all-about-variables/var-declarations',
+                        status: 'to-do',
+                        subLinks: [
+                            {
+                                name: 'var',
+                                href: '/js/all-about-variables/var-declarations/var',
+                            },
+                            {
+                                name: 'let',
+                                href: '/js/all-about-variables/var-declarations/let',
+                            },
+                            {
+                                name: 'const',
+                                href: '/js/all-about-variables/var-declarations/const',
+                            },
+                        ],
+                    },
+                ],
+            },
         ],
     },
-    { name: 'React', href: '/react', status: 'to-do'},
+    { name: 'React', href: '/react', status: 'to-do' },
 ];
 
 export default links;
